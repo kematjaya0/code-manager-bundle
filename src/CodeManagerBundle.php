7fd61c63e8c671b175bbbf9c0b418378676cd5ee
Nur Hidayatullah <kematjaya0@gmail.com>
2020-12-17 07:50:09 +0700

    initialize class

---
+++ src/CodeManagerBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Kematjaya\CodeManagerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Nur Hidayatullah <kematjaya0@gmail.com>
+ */
+class CodeManagerBundle extends Bundle
+{
+    
+}
